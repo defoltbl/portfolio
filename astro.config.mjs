7f929a9e1e9ee2827f3b0fa7c94e6a246139c9b3
Andrii Maksymenko <studentuknow@gmail.com>
2026-06-30 14:00:44 +0200

    Update public CV and set production domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 
-// If you deploy to a custom domain (e.g. andriimaksymenko.dev), set:
-// site: 'https://andriimaksymenko.dev'
+
 export default defineConfig({
-  site: 'https://andriimaksymenko.dev',
+  site: 'https://andrii-maksymenko.com',
 });
